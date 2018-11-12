6d21b0a04bf70ec6e5dd8e218723500c97d80da1
Iamoscarbc <oscarmbravoc@gmail.com>
2018-11-11 22:37:03 -0500

    BD actualizada

--- ProyectoJN/Base de Datos/jn.sql
+++ ProyectoJN/Base de Datos/jn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-10-2018 a las 03:16:03
+-- Tiempo de generación: 12-11-2018 a las 04:36:17
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.10
 
@@ -45,9 +45,11 @@ CREATE TABLE `alumno` (
 --
 
 INSERT INTO `alumno` (`idAlumno`, `DNI`, `Nombres`, `Apellidos`, `Direccion`, `Telefono`, `Edad`, `Grado`, `Sexo`) VALUES
-(1, '73260012', 'Oscar Manuel', 'Huamani Human', 'Av.Los Andes 189', '946448040', '19', '3 años', 'Masculino'),
-(3, '45345656', 'Oscar Manuel', 'Bravo Carbajal', 'Av.Los Andes 187', '946448040', '12', '3 años', 'Masculino'),
-(8, '73260012', 'Adriano', 'Bravo', 'Los pinos', '123456789', '20', '3 años', 'Femenino');
+(3, '45345654', 'Oscar Manuel', 'Bravo Carbajal', 'Av.Los Andes 187', '946448040', '3', '5 años', 'Masculino'),
+(8, '73260512', 'Pedro', 'Bravo', 'Jr. Francisco Bolognesi 333', '123456789', '3', '3 años', 'Femenino'),
+(41, '45626545', 'Antonio', 'Alvarez Castro', 'Av.Santa Luisa 123', '958956565', '3', '3 años', 'Masculino'),
+(53, '75343327', 'Juan Diego', 'Herrera Gomez', 'Jr. Los Andes 136', '949726521', '20', '5 años', 'Masculino'),
+(55, '74455896', 'Rafael', 'Cordova', 'Jr.Los Andes 189', '+51956727151', '5', 'Estimulación Temprana', 'Masculino');
 
 -- --------------------------------------------------------
 
@@ -65,13 +67,15 @@ CREATE TABLE `curso` (
 --
 
 INSERT INTO `curso` (`idCurso`, `Nombre`) VALUES
-(1, 'Logico Matematico'),
-(2, 'Comunicacion Integral'),
+(9, 'Arte'),
 (3, 'Ciencia  y Ambiente'),
-(4, 'Personal Social'),
 (5, 'Computo'),
-(6, 'Minichef'),
+(2, 'Comunicacion Integral'),
+(10, 'Educación Fisica'),
 (7, 'Ingles'),
+(1, 'Logico Matematico'),
+(6, 'Minichef'),
+(4, 'Personal Social'),
 (8, 'Tutoria');
 
 -- --------------------------------------------------------
@@ -97,8 +101,8 @@ CREATE TABLE `empleado` (
 --
 
 INSERT INTO `empleado` (`idEmpleado`, `idUsuario`, `DNI`, `Nombres`, `Apellidos`, `Direccion`, `Telefono`, `Edad`, `Sexo`) VALUES
-(3, 1, '73260012', 'Oscar Manuel', 'Bravo Carbajal', 'Jr.Los Andes 189', '956727151', '19', 'Masculino'),
-(4, 1, '73260012', 'Oscar Manuel', 'Bravo Carbajal', 'Jr.Los Andes 189', '956727151', '19', 'Femenino');
+(20, 1, '73260012', 'Oscar Manuel', 'Bravo Carbajal', 'Jr.Los Andes 187', '946448040', '19', 'Masculino'),
+(24, 1, '45456456', 'Vilma Haydee', 'Bravo Huamash', 'Jr. Francisco Bolognesi 333', '970356485', '54', 'Femenino');
 
 -- --------------------------------------------------------
 
@@ -110,8 +114,19 @@ CREATE TABLE `matricula` (
   `idMatricula` int(11) NOT NULL,
   `idEmpleado` int(10) NOT NULL,
   `idAlumno` int(10) NOT NULL,
+  `idPagos` int(8) NOT NULL,
   `Fecha_Matricula` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `matricula`
+--
+
+INSERT INTO `matricula` (`idMatricula`, `idEmpleado`, `idAlumno`, `idPagos`, `Fecha_Matricula`) VALUES
+(5, 20, 3, 4, '2018-11-29'),
+(6, 20, 3, 4, '2018-11-22'),
+(7, 20, 41, 5, '2018-11-15'),
+(8, 20, 3, 4, '2018-11-21');
 
 -- --------------------------------------------------------
 
@@ -134,8 +149,7 @@ CREATE TABLE `notas` (
 --
 
 INSERT INTO `notas` (`idNotas`, `idCurso`, `idAlumno`, `Bim_1`, `Bim_2`, `Bim_3`, `Bim_4`) VALUES
-(3, 1, 1, '17', '17', '17', '17'),
-(4, 4, 1, '17', '17', '17', '17');
+(47, 10, 41, '17', '17', '17', '17');
 
 -- --------------------------------------------------------
 
@@ -157,8 +171,10 @@ CREATE TABLE `pagos` (
 --
 
 INSERT INTO `pagos` (`idPagos`, `idAlumno`, `DNI_R`, `Concepto`, `Importe`, `Fecha_Pago`) VALUES
-(2, 1, '58694712', 'Pension', '300', '20/06/2018'),
-(3, 8, '73260012', 'Matricula', '250', '2018-10-04');
+(3, 8, '73260012', 'Matricula', '250', '2018-10-04'),
+(4, 3, '45626565', 'Pension', '300', '2018-10-04'),
+(5, 41, '54826565', 'Pension', '300', '2018-10-04'),
+(6, 3, '73260012', 'Matricula', '250', '2018-11-08');
 
 -- --------------------------------------------------------
 
@@ -197,8 +213,22 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`idUsuario`, `idTipo`, `Usuario`, `Password`) VALUES
-(1, 1, 'VilmaBH', '123456'),
-(2, 2, 'OscarBH', '123456');
+(1, 2, 'VilmaBH', '123456'),
+(2, 1, 'OscarBH', '123456'),
+(3, 1, 'RafaelLC', '123456'),
+(4, 1, 'CristianPE', '123456'),
+(5, 1, 'StaceyCI', '123456'),
+(6, 2, 'AntonioLC', '123456'),
+(7, 2, 'CarlosPE', '123456'),
+(8, 2, 'SofiaCI', '123456'),
+(9, 2, 'RubiLC', '123456'),
+(10, 2, 'CarolPE', '123456'),
+(11, 2, 'SaulCI', '123456'),
+(12, 2, 'RodrigoLC', '123456'),
+(13, 2, 'ConniePE', '123456'),
+(14, 2, 'PedroCI', '123456'),
+(15, 2, 'PercyLC', '123456'),
+(16, 2, 'JesusPE', '123456');
 
 --
 -- Índices para tablas volcadas
@@ -214,7 +244,8 @@ ALTER TABLE `alumno`
 -- Indices de la tabla `curso`
 --
 ALTER TABLE `curso`
-  ADD PRIMARY KEY (`idCurso`);
+  ADD PRIMARY KEY (`idCurso`),
+  ADD KEY `Nombre` (`Nombre`);
 
 --
 -- Indices de la tabla `empleado`
@@ -229,7 +260,8 @@ ALTER TABLE `empleado`
 ALTER TABLE `matricula`
   ADD PRIMARY KEY (`idMatricula`),
   ADD KEY `fk_Matricula_Empleado1_idx` (`idEmpleado`),
-  ADD KEY `fk_Matricula_Alumno1_idx` (`idAlumno`);
+  ADD KEY `fk_Matricula_Alumno1_idx` (`idAlumno`),
+  ADD KEY `fk_Matricula_Pagos1` (`idPagos`);
 
 --
 -- Indices de la tabla `notas`
@@ -267,37 +299,37 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `alumno`
 --
 ALTER TABLE `alumno`
-  MODIFY `idAlumno` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idAlumno` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT de la tabla `empleado`
 --
 ALTER TABLE `empleado`
-  MODIFY `idEmpleado` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idEmpleado` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `matricula`
 --
 ALTER TABLE `matricula`
-  MODIFY `idMatricula` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idMatricula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `notas`
 --
 ALTER TABLE `notas`
-  MODIFY `idNotas` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idNotas` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT de la tabla `pagos`
 --
 ALTER TABLE `pagos`
-  MODIFY `idPagos` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idPagos` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idUsuario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
@@ -314,7 +346,8 @@ ALTER TABLE `empleado`
 --
 ALTER TABLE `matricula`
   ADD CONSTRAINT `fk_Matricula_Alumno1` FOREIGN KEY (`idAlumno`) REFERENCES `alumno` (`idAlumno`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Matricula_Empleado1` FOREIGN KEY (`idEmpleado`) REFERENCES `empleado` (`idEmpleado`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Matricula_Empleado1` FOREIGN KEY (`idEmpleado`) REFERENCES `empleado` (`idEmpleado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Matricula_Pagos1` FOREIGN KEY (`idPagos`) REFERENCES `pagos` (`idPagos`);
 
 --
 -- Filtros para la tabla `notas`
